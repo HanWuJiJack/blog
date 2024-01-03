4d2a97aeffa2feb96fa6b948473a93b44c2b36b9
Hsueh <79571635@qq.com>
2024-01-03 15:08:04 +0800

    构建 谷歌统计分析脚本

--- plugins/ga.js
+++ plugins/ga.js
@@ -2,21 +2,22 @@ if (process.client && process.env.ENV_ === 'pro') {
   /*
    ** Google 统计分析脚本
    */
-  (function (i, s, o, g, r, a, m) {
-    i.GoogleAnalyticsObject = r;
-    i[r] = i[r] || function () {
-      (i[r].q = i[r].q || []).push(arguments)
-    }, i[r].l = 1 * new Date();
-    a = s.createElement(o),
-      m = s.getElementsByTagName(o)[0];
+  (function () {
+    var a = document.createElement('script');
     a.async = 1;
-    a.src = g;
-    m.parentNode.insertBefore(a, m)
-  })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga')
+    a.src = "https://www.googletagmanager.com/gtag/js?id=G-N8464N6KV6";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+  })()
   /*
    ** 当前页的访问统计
    */
-  ga('create', 'G-N8464N6KV6', 'auto')
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+  gtag('config', 'G-N8464N6KV6');
 }
 
 export default ({
@@ -33,8 +34,12 @@ export default ({
       /*
        ** 告诉 GA 增加一个 PV
        */
-      ga('set', 'page', to.fullPath)
-      ga('send', 'pageview')
+       window.dataLayer = window.dataLayer || [];
+       function gtag() {
+         dataLayer.push(arguments);
+       }
+       gtag('js', new Date());
+       gtag('config', 'G-N8464N6KV6');
     }
   })
 }
