f9ee6222aadb6d40b4184c0d46d03a8125b193c5
Hsueh <79571635@qq.com>
2024-01-04 15:43:10 +0800

    更新上个人信息

--- pages/info.vue
+++ pages/info.vue
@@ -1,217 +1,221 @@
 <template>
-  <div class="Box">
-    <div class="userInfo">
-      <div>
-        <span class="title">
-          Hsueh
-        </span>
-      </div>
-      <div>
-        <span class="text">
-          求职意向：java go node 后端开发
-        </span>
-        <span class="text">
-          工作开始时间：2018年11月
-        </span>
-        <span class="text">
-          性别：男
-        </span>
-      </div>
-      <div>
-        <span class="text">
-          出生年月：1997年1月
-        </span>
-        <span class="text">
-          邮箱：79571635@qq.com
-        </span>
-        <!-- <span class="text">
+  <div class="body">
+    <el-button @click="pdfDown_" class="exportButton">导出</el-button>
+    <div class="Box" ref="box">
+      <div class="userInfo">
+        <div>
+          <span class="title">
+            Hsueh
+          </span>
+        </div>
+        <div>
+          <span class="text">
+            求职意向：java go node 后端开发
+          </span>
+          <span class="text">
+            工作开始时间：2018年11月
+          </span>
+          <span class="text">
+            性别：男
+          </span>
+        </div>
+        <div>
+          <span class="text">
+            出生年月：1997年1月
+          </span>
+          <span class="text">
+            邮箱：79571635@qq.com
+          </span>
+          <!-- <span class="text">
           联系电话：
         </span> -->
+        </div>
       </div>
-    </div>
-    <span class="box_title">
-      Web前端开发经历
-    </span>
-    <div class="box_head">
-      <span class="text_ box_head_title">2021.06-至今</span>
-      <span class="text_ box_head_title">上海海骄有限公司</span>
-    </div>
-    <div>
-      <p class="text_">
-        工作内容：
-      </p>
-      <p class="text__" style="padding-left: 20px">
-        1、新系统搭建，负责新系统前端从0到1搭建；
-      </p>
-      <p class="text__" style="padding-left: 20px">
-        2、新功能开发，主要指综合管理系统新增功能模块开发、h5展示页面开发
-      </p>
-      <p class="text__" style="padding-left: 20px">
-        3、维护系统包含：
-        <br />
-        新系统：综合管理后台系统（OA审批流（动态组件）、大文件上传（分片上传））、
-        <br />
-        遗留系统：公司官网、相控阵后台管理系统、远传后台管理系统（管道检查相关）、售后管理系统和售后小程序（主要包含设备卖出后年检和售后记录）
-      </p>
-      <p class="text__" style="padding-left: 20px">
-        4、参与需求评审会和产品经理,后端讨论项目如何具体落地；
-      </p>
-      <p class="text__" style="padding-left: 20px">
-        5、和后端定义好数据结构和具体字段，mock数据进行前端开发；
-      </p>
-      <p class="text_">
-        主要技术栈：
-      </p>
-      <p class="text__" style="padding-left: 20px">
-        综合管理后台系统、远传后台管理系统、售后管理系统、H5：Vue+VueRouter+Vuex+Element+Axios+Eslint+Less
-        <br />
-        相控阵后台管理系统：React+ReactRouterDom+ReactRedux+ReduxThunk+Axios+Typescript+Eslint+Less
-        <br />
-        售后小程序：微信小程序原生开发
-      </p>
-    </div>
-    <div class="box_head">
-      <span class="text_ box_head_title">2020.10-2021.06</span>
-      <span class="text_ box_head_title">上海致维网络有限公司</span>
-    </div>
-    <div>
-      <p class="text_">
-        工作内容：
-      </p>
-      <p class="text__" style="padding-left: 20px">
-        1、新功能开发，主要指母婴综合管理系统和智零售App新增功能模块开发
-      </p>
-      <p class="text__" style="padding-left: 20px">
-        2、维护系统包含：
-        <br />
-        母婴综合管理系统、智零售App、nb3后台管理系统（用于门店管理智能卖货的小程序和智零售App）
-      </p>
-      <p class="text__" style="padding-left: 20px">
-        3、参与需求评审会和产品经理,后端讨论项目如何具体落地；并且给出项目经理具体开发时间。
-      </p>
-      <p class="text__" style="padding-left: 20px">
-        4、和后端定义好数据结构和具体字段，mock数据进行前端开发；
-      </p>
-      <p class="text_">
-        主要技术栈：
-      </p>
-      <p class="text__" style="padding-left: 20px">
-        母婴综合管理系统、nb3后台管理系统：Angularjs(1.5.6)+Material+Nginx
-        <br />
-        智零售App：ReactNativ（React+reactNavigation+ReactRedux+reduxSaga+Fetch+Less）
-      </p>
-    </div>
-    <div class="box_head">
-      <span class="text_ box_head_title">2018.11-2020.09</span>
-      <span class="text_ box_head_title">上海星轨网络有限公司 </span>
-    </div>
-    <div>
-      <p class="text_">
-        工作内容：
-      </p>
-      <p class="text__" style="padding-left: 20px">
-        1、以产品经理的身份和用户对接需求，和ui设计师对接页面展示
-      </p>
-      <p class="text__" style="padding-left: 20px">
-        2、新系统搭建，负责新系统前端从0到1搭建包含：
-        <br />
-        移动端：h5，微信小程序；
-        <br />
-        pc端：后台管理系统、公司官网
-      </p>
-      <p class="text__" style="padding-left: 20px">
-        3、维护系统包含：
-        <br />
-        律师事务所前台和后台系统、青山回收、艾兰岛、B&W官网等
-      </p>
-      <p class="text__" style="padding-left: 20px">
-        4、和后端定义好数据结构和具体字段，mock数据进行前端开发；
-      </p>
-      <p class="text_">
-        主要技术栈：
-      </p>
-      <p class="text__" style="padding-left: 20px">
-        律师事务所前台和后台系统：
-        前台：原生+jquery；
-        后台：vue+jquery；
-        <br />
-        艾兰岛、青山回收后台管理系统：React+ReactRouter+ReactRedux+ReduxSaga+Axios;<br />
-        青山回收小程序：Taro(开放式跨端跨框架解决方案)+React全家桶;<br />
-        B&W官网:express+express-art-template+jquery；
-      </p>
-    </div>
-    <span class="box_title">
-      技能点
-    </span>
-    <div>
-      <p class="text_">
-        工作常用：
-      </p>
-      <p class="text__" style="padding-left: 20px">
-        vue2和vue3(Vue、VueRouter、Vuex、Element、Axios、Eslint、Less)<br />
-        React(React、ReactRouterDom、ReactRedux、ReduxThunk、ReduxSaga、Axios、Typescript、Eslint、Less)<br />
-        ReactNative<br />
-        微信小程序原生开发<br />
-        Nginx<br />
-        webPack<br />
-      </p>
-      <p class="text_">
-        自学积累：
-      </p>
-      <p class="text__" style="padding-left: 20px">
-        Electron<br />
-        Koa(node框架)+Sequelize(基于 promise 的 Node.js ORM)<br />
-        Adonis(node框架)<br />
-        Mysql（57:latin1/80：utf8mb4）<br />
-        Mongodb<br />
-        Redis<br />
-        Linux系统（centos 7）<br />
-        Java基础、spring、springMVC、mybatis+mybatisPuls、springBoot<br />
-        Rabbitmq<br />
-        <br />
-      </p>
-    </div>
-    <span class="box_title">
-      自我评价
-    </span>
-    <div>
-      <p class="text_">
-        为人真诚，谦虚好学，乐观开朗，能很好地处理人际关系，遇到问题不抱怨而是用行动去解决，
-        有较强的责任心和团队合作精神；对自己要求严格，做事情讲究高质量。
-      </p>
-    </div>
-    <span class="box_title">
-      教育背景
-    </span>
-    <div class="box_head">
-      <span class="text_ box_head_title">2016.09-2019.07 </span>
-      <span class="text_ box_head_title">安徽机电职业技术学院</span>
-      <span class="text_ box_head_title">大专</span>
-    </div>
-    <div class="box_head">
-      <span class="text_ box_head_title">2022.03-2024.07 </span>
-      <span class="text_ box_head_title">阜阳师范大学</span>
-      <span class="text_ box_head_title">本科</span>
-    </div>
-    <span class="box_title">
-      开源
-    </span>
-    <div>
-      <p class="text__" style="cursor: pointer">
-        <a href="https://github.com/HanWuJiJack" target="_blank" rel="noopener noreferrer">
-          <span>github</span>&nbsp;&nbsp;
-          <img width="30" :src={githubPng} alt="" />
-        </a>
-      </p>
-      <p class="text__" style="padding-top: 20px, cursor: pointer">
-        <!-- <a href="https://s1.z100.vip:8555/vue/welcome" target="_blank" rel="noopener noreferrer">开源项目体验地址</a> -->
-      </p>
+      <span class="box_title">
+        Web前端开发经历
+      </span>
+      <div class="box_head">
+        <span class="text_ box_head_title">2021.06-至今</span>
+        <span class="text_ box_head_title">上海海骄有限公司</span>
+      </div>
+      <div>
+        <p class="text_">
+          工作内容：
+        </p>
+        <p class="text__" style="padding-left: 20px">
+          1、新系统搭建，负责新系统前端从0到1搭建；
+        </p>
+        <p class="text__" style="padding-left: 20px">
+          2、新功能开发，主要指综合管理系统新增功能模块开发、h5展示页面开发
+        </p>
+        <p class="text__" style="padding-left: 20px">
+          3、维护系统包含：
+          <br />
+          新系统：综合管理后台系统（OA审批流（动态组件）、大文件上传（分片上传））、
+          <br />
+          遗留系统：公司官网、相控阵后台管理系统、远传后台管理系统（管道检查相关）、售后管理系统和售后小程序（主要包含设备卖出后年检和售后记录）
+        </p>
+        <p class="text__" style="padding-left: 20px">
+          4、参与需求评审会和产品经理,后端讨论项目如何具体落地；
+        </p>
+        <p class="text__" style="padding-left: 20px">
+          5、和后端定义好数据结构和具体字段，mock数据进行前端开发；
+        </p>
+        <p class="text_">
+          主要技术栈：
+        </p>
+        <p class="text__" style="padding-left: 20px">
+          综合管理后台系统、远传后台管理系统、售后管理系统、H5：Vue+VueRouter+Vuex+Element+Axios+Eslint+Less
+          <br />
+          相控阵后台管理系统：React+ReactRouterDom+ReactRedux+ReduxThunk+Axios+Typescript+Eslint+Less
+          <br />
+          售后小程序：微信小程序原生开发
+        </p>
+      </div>
+      <div class="box_head">
+        <span class="text_ box_head_title">2020.10-2021.06</span>
+        <span class="text_ box_head_title">上海致维网络有限公司</span>
+      </div>
+      <div class="break_page">
+        <p class="text_">
+          工作内容：
+        </p>
+        <p class="text__" style="padding-left: 20px">
+          1、新功能开发，主要指母婴综合管理系统和智零售App新增功能模块开发
+        </p>
+        <p class="text__" style="padding-left: 20px">
+          2、维护系统包含：
+          <br />
+          母婴综合管理系统、智零售App、nb3后台管理系统（用于门店管理智能卖货的小程序和智零售App）
+        </p>
+        <p class="text__" style="padding-left: 20px">
+          3、参与需求评审会和产品经理,后端讨论项目如何具体落地；并且给出项目经理具体开发时间。
+        </p>
+        <p class="text__" style="padding-left: 20px">
+          4、和后端定义好数据结构和具体字段，mock数据进行前端开发；
+        </p>
+        <p class="text_">
+          主要技术栈：
+        </p>
+        <p class="text__" style="padding-left: 20px">
+          母婴综合管理系统、nb3后台管理系统：Angularjs(1.5.6)+Material+Nginx
+          <br />
+          智零售App：ReactNativ（React+reactNavigation+ReactRedux+reduxSaga+Fetch+Less）
+        </p>
+      </div>
+      <div class="box_head">
+        <span class="text_ box_head_title">2018.11-2020.09</span>
+        <span class="text_ box_head_title">上海星轨网络有限公司 </span>
+      </div>
+      <div>
+        <p class="text_">
+          工作内容：
+        </p>
+        <p class="text__" style="padding-left: 20px">
+          1、以产品经理的身份和用户对接需求，和ui设计师对接页面展示
+        </p>
+        <p class="text__" style="padding-left: 20px">
+          2、新系统搭建，负责新系统前端从0到1搭建包含：
+          <br />
+          移动端：h5，微信小程序；
+          <br />
+          pc端：后台管理系统、公司官网
+        </p>
+        <p class="text__" style="padding-left: 20px">
+          3、维护系统包含：
+          <br />
+          律师事务所前台和后台系统、青山回收、艾兰岛、B&W官网等
+        </p>
+        <p class="text__" style="padding-left: 20px">
+          4、和后端定义好数据结构和具体字段，mock数据进行前端开发；
+        </p>
+        <p class="text_">
+          主要技术栈：
+        </p>
+        <p class="text__" style="padding-left: 20px">
+          律师事务所前台和后台系统：
+          前台：原生+jquery；
+          后台：vue+jquery；
+          <br />
+          艾兰岛、青山回收后台管理系统：React+ReactRouter+ReactRedux+ReduxSaga+Axios;<br />
+          青山回收小程序：Taro(开放式跨端跨框架解决方案)+React全家桶;<br />
+          B&W官网:express+express-art-template+jquery；
+        </p>
+      </div>
+      <span class="box_title">
+        技能点
+      </span>
+      <div class="break_page_2">
+        <p class="text_">
+          工作常用：
+        </p>
+        <p class="text__" style="padding-left: 20px">
+          vue2和vue3(Vue、VueRouter、Vuex、Element、Axios、Eslint、Less)<br />
+          React(React、ReactRouterDom、ReactRedux、ReduxThunk、ReduxSaga、Axios、Typescript、Eslint、Less)<br />
+          ReactNative<br />
+          微信小程序原生开发<br />
+          Nginx<br />
+          webPack<br />
+        </p>
+        <p class="text_">
+          自学积累：
+        </p>
+        <p class="text__" style="padding-left: 20px">
+          Electron<br />
+          Koa(node框架)+Sequelize(基于 promise 的 Node.js ORM)<br />
+          Adonis(node框架)<br />
+          Mysql（57:latin1/80：utf8mb4）<br />
+          Mongodb<br />
+          Redis<br />
+          Linux系统（centos 7）<br />
+          Java基础、spring、springMVC、mybatis+mybatisPuls、springBoot<br />
+          Rabbitmq<br />
+          <br />
+        </p>
+      </div>
+      <span class="box_title">
+        自我评价
+      </span>
+      <div>
+        <p class="text_">
+          为人真诚，谦虚好学，乐观开朗，能很好地处理人际关系，遇到问题不抱怨而是用行动去解决，
+          有较强的责任心和团队合作精神；对自己要求严格，做事情讲究高质量。
+        </p>
+      </div>
+      <span class="box_title">
+        教育背景
+      </span>
+      <div class="box_head">
+        <span class="text_ box_head_title">2016.09-2019.07 </span>
+        <span class="text_ box_head_title">安徽机电职业技术学院</span>
+        <span class="text_ box_head_title">大专</span>
+      </div>
+      <div class="box_head">
+        <span class="text_ box_head_title">2022.03-2024.07 </span>
+        <span class="text_ box_head_title">阜阳师范大学</span>
+        <span class="text_ box_head_title">本科</span>
+      </div>
+      <span class="box_title">
+        开源
+      </span>
+      <div>
+        <p class="text__" style="cursor: pointer">
+          <a href="https://github.com/HanWuJiJack" target="_blank" rel="noopener noreferrer">
+            <span>github</span>&nbsp;&nbsp;
+            <img width="30" src="@/assets/images/GitHub.png" alt="" />
+          </a>
+        </p>
+        <p class="text__" style="padding-top: 20px, cursor: pointer">
+          <!-- <a href="https://s1.z100.vip:8555/vue/welcome" target="_blank" rel="noopener noreferrer">开源项目体验地址</a> -->
+        </p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import list from '../components/articleList'
+import htmlToPdf from '../utils/htmlToPdf.js'
 export default {
   name: 'index',
   layout: 'nor',
@@ -235,12 +239,14 @@ export default {
   data() {
     return {
       res: {},
-      githubPng:"@/"
     }
   },
   computed: {
   },
   methods: {
+    pdfDown_() {
+      htmlToPdf('xxx', this.$refs.box)
+    }
   },
   mounted() {
 
@@ -248,29 +254,29 @@ export default {
 }
 </script>
 <style scoped lang="scss">
+.body {
+  position: relative;
+  margin: 0 auto;
+  width: 900px;
+  .exportButton {
+    display: inline-block;
+    position: absolute;
+    width: 80px;
+    top: 20px;
+    right: 20px;
+    box-sizing: border-box;
+  }
+}
 .Box {
-  max-width: 1200px;
-  width: 100%;
   box-sizing: border-box;
   font-family: myFirstFont, "Microsoft YaHei", "微软雅黑", "Lantinghei SC", "Open Sans", Arial, "Hiragino Sans GB", "STHeiti", "WenQuanYi Micro Hei", SimSun, sans-serif;
   background-color: #fff;
   padding: 30px 50px;
-
-  .exportButton {
-    width: 80px;
-    position: fixed;
-    top: 30px;
-    right: 0px;
-    left: 0;
-    margin: 0 auto;
-    transform: translateX(430px);
-    box-sizing: border-box;
-  }
-
+ 
   .userInfo {
     border: 2px solid #4395ff;
     padding: 20px;
-    border-left: 150px solid #4395ff;
+    // border-left: 150px solid #4395ff;
     box-sizing: border-box;
     border-radius: 10px;
   }
@@ -300,11 +306,11 @@ export default {
   .box_title {
     display: inline-block;
     font-family: myFirstFont, "Microsoft YaHei", "微软雅黑", "Lantinghei SC", "Open Sans", Arial, "Hiragino Sans GB", "STHeiti", "WenQuanYi Micro Hei", SimSun, sans-serif;
-    font-size: 25px;
+    font-size: 18px;
     background-color: #4395ff;
     color: #fff;
     box-sizing: border-box;
-    padding: 20px;
+    padding:10px 20px;
     margin: 20px 0;
     border-radius: 10px;
   }
@@ -319,4 +325,5 @@ export default {
   }
 
   // #4395ff
-}</style>
+}
+</style>
