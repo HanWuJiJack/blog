70898c2dc18e36d47aa35132a3f16b56460a1290
Hsueh <79571635@qq.com>
2024-01-18 14:48:44 +0800

    优化代码

--- pages/three.vue
+++ pages/three.vue
@@ -71,7 +71,6 @@ export default {
     }
     function debounce(func, wait) {
       let timeout;
-
       return function () {
         let context = this; // 保存this指向
         let args = arguments; // 拿到event对象
@@ -82,7 +81,6 @@ export default {
         }, wait);
       }
     }
-    // 加载图片资源
     const deep = 5000
     let width = window.innerWidth
     let height = window.innerHeight
@@ -93,41 +91,25 @@ export default {
     window.onresize = debounce(function () {
       width = window.innerWidth
       height = window.innerHeight
-      // 更新运动星体xy尺寸
       pointsCubes.forEach((pointsCube) => {
         const x = THREE.MathUtils.randFloat(0, width).toFixed(2);
         const y = THREE.MathUtils.randFloat(0, height).toFixed(2);
         pointsCube.position.x = x
         pointsCube.position.y = y
       })
-      // 更新闪烁星体xy尺寸
       pointsCubes_.forEach((pointsCube) => {
         const x = THREE.MathUtils.randFloat(0, width).toFixed(2);
         const y = THREE.MathUtils.randFloat(0, height).toFixed(2);
         pointsCube.position.x = x
         pointsCube.position.y = y
       })
-      // 更新地球xy
       meshEarth.position.set(width * 0.8, height * 0.8, deep - 100)
-
-      // 更新光源xyz
       pointLight.position.set(100, height - 100, deep - 300);
-
-      // 更新空间盒子xyz
       mesh.position.set(width / 2, height / 2, deep / 2)
-      // 更新空间盒子尺寸
       mesh.scale.set(width / initWidth, height / initHeight, 1);
-
-
-      // 更新相机
       camera.fov = Math.tan(height / 2 / deep) / Math.PI * 180 * 1.9
-      // 全屏情况下：设置观察范围长宽比aspect为窗口宽高比
       camera.aspect = width / height;
-      // 渲染器执行render方法的时候会读取相机对象的投影矩阵属性projectionMatrix
-      // 但是不会每渲染一帧，就通过相机的属性计算投影矩阵(节约计算资源)
-      // 如果相机的一些属性发生了变化，需要执行updateProjectionMatrix ()方法更新相机的投影矩阵
       camera.updateProjectionMatrix();
-      // 重置渲染器输出画布canvas尺寸
       renderer.setSize(width, height);
       render3d.setSize(width, height);
       camera.lookAt(width / 2, height / 2, deep)
@@ -141,20 +123,11 @@ export default {
     const camera = new THREE.PerspectiveCamera(Math.tan(height / 2 / deep) / Math.PI * 180 * 1.9, width / height, 1000, 3000000);
     camera.position.set(width / 2, height / 2, 0)
     scene.add(camera)
-
-
-    // 创建环境
-    // 创建结构
     const geometry = new THREE.BoxGeometry(width, height, deep)
-    // 创建材质
-    // color: '#251631'
-    // color: '#000'
     const material = new THREE.MeshPhongMaterial({ side: THREE.BackSide, color: '#333' })
     const mesh = new THREE.Mesh(geometry, material)
     mesh.position.set(width / 2, height / 2, deep / 2)
     scene.add(mesh)
-
-    // 创建地球
     const geometryEarth = new THREE.SphereGeometry(50);
     const materialEarth = new THREE.MeshPhongMaterial({
       shininess: 50,//.specular高亮的程度，越高的值越闪亮。默认值为 30。
@@ -176,8 +149,6 @@ export default {
     objcss3d.position.x = 0
     objcss3d.position.y = 0
     objcss3d.position.z = 0
-    // objcss3d.scale.set(1, 1, 1)
-    // scene.add(objcss3d)
     meshEarth.add(objcss3d)
     scene.add(meshEarth)
     objcss3d.visible = false
@@ -206,10 +177,6 @@ export default {
     pointLight.decay = 0.8;//设置光源不随距离衰减
     pointLight.position.set(100, height - 100, deep - 300);
     scene.add(pointLight);
-
-    // const pointLightHelper = new THREE.PointLightHelper(pointLight, 10, 0xffffff)
-    // scene.add(pointLightHelper);
-
     const Textures = [
       new THREE.TextureLoader().load(`${this.publicPath}/end/1.png`),
       new THREE.TextureLoader().load(`${this.publicPath}/end/5.png`),
@@ -301,7 +268,6 @@ export default {
       }
     }
     const percentFn_ = percentFn()
-
     material.map = new THREE.TextureLoader().load(`${this.publicPath}/end/bg1.jpg`, (texture) => {
       percentFn_.add()
       percentFn_.tu()
@@ -312,7 +278,6 @@ export default {
     })
 
     const renderer = new THREE.WebGLRenderer({
-      // 设置对数深度缓冲区，优化深度冲突问题
       logarithmicDepthBuffer: true
     });
     const canvas = renderer.domElement
@@ -336,9 +301,6 @@ export default {
     render3d.domElement.style.left = '0px';
     render3d.domElement.style.zIndex = 3;//设置层级
     render3d.domElement.style.pointerEvents = 'none';
-    // const controls = new OrbitControls(camera, render3d.domElement);
-
- 
     const composer = new EffectComposer(renderer);
     const renderPass = new RenderPass(scene, camera);
     composer.addPass(renderPass);
@@ -370,58 +332,29 @@ export default {
         audio = null
       });
     }
-    // //模型描边颜色，默认白色         
     outlinePass.visibleEdgeColor.set(0xffff00);
-    // //高亮发光描边厚度
     outlinePass.edgeThickness = 4;
-    // //高亮描边发光强度
     outlinePass.edgeStrength = 6;
-    //  //模型闪烁频率控制，默认0不闪烁
-    outlinePass.pulsePeriod = 2;
-    //模型边缘高亮边框颜色，默认白色  
+    outlinePass.pulsePeriod = 2; 
     composer.addPass(outlinePass);
-    // -------添加描边效果---------
-
-    // -------射线---------
     renderer.domElement.addEventListener('click', function (event) {
       const objects = getSceneModelFaceNum(scene)
-      // .offsetY、.offsetX以canvas画布左上角为坐标原点,单位px
       const px = event.offsetX;
       const py = event.offsetY;
-      //屏幕坐标px、py转WebGL标准设备坐标x、y
-      //width、height表示canvas画布宽高度
       const x = (px / width) * 2 - 1;
       const y = -(py / height) * 2 + 1;
-      //创建一个射线投射器`Raycaster`
       const raycaster = new THREE.Raycaster();
-      //.setFromCamera()计算射线投射器`Raycaster`的射线属性.ray
-      // 形象点说就是在点击位置创建一条射线，射线穿过的模型代表选中
       raycaster.setFromCamera(new THREE.Vector2(x, y), camera);
-      //.intersectObjects([mesh1, mesh2, mesh3])对参数中的网格模型对象进行射线交叉计算
-      // 未选中对象返回空数组[],选中一个对象，数组1个元素，选中两个对象，数组两个元素
       const intersects = raycaster.intersectObjects(objects);
       console.log("射线器返回的对象", intersects);
-      // intersects.length大于0说明，说明选中了模型
       if (intersects.length > 0) {
-        // 选中模型的第一个模型，设置为红色
-        // intersects[0].object.material.color.set(0xff0000);
         intersects[0].object.change()
       }
     })
-    // -------射线---------
-
-    // const controls = new OrbitControls(camera, canvas);
-    // 监听拖动变化
-    // controls.addEventListener("change", () => {
-    // 	camera.lookAt(width / 2, height / 2, deep)
-    // })
-    // renderer.render(scene, camera)
-
     const color = new THREE.Color()
     const colors = [color.set("#084c68").clone(), color.set("#ffffff").clone(), color.set("#738391").clone()]
 
     let num = 0
-    // 让立方体动起来
     function animate() {
       if (objcss3dtitle.position.x < width) {
         objcss3dtitle.position.x += 1
@@ -435,7 +368,7 @@ export default {
           percentNumW++;
           percentNum++;
           percentFn_.tu()
-          console.log("percentNum", percentNum)
+          // console.log("percentNum", percentNum)
         }
         // 星体闪烁
         pointsCubes_.forEach((pointsCube) => {
