8fca624f332fd9d2062496aa83d08a14cace09ae
Hsueh <79571635@qq.com>
2024-01-20 14:19:11 +0800

    更新时间

--- components/appFooter.vue
+++ components/appFooter.vue
@@ -2,7 +2,7 @@
   <footer class="app-footer">
     <p>Powered By <a href="https://zh.nuxtjs.org/" rel="nofollow" target="_blank">Nuxt.js</a>，Designed By <a href="https://github.com/HanWuJiJack" target="_blank">
         Hsueh</a></p>
-    <p class="copyright">Copyright© 2020-{{ currentYear }}</p>
+    <p class="copyright">Copyright© 2023-{{ currentYear }}</p>
     <!-- <div style="height: 17px;">
       <a target="_blank" rel="nofollow" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=50011702500392"
          style="display:inline-block;text-decoration:none;height:20px;line-height:20px;">
